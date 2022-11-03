39827d1020e01570c24e4147ba47b6c4be520dbe
БорисоваЕА <@211-03>
2022-11-03 12:19:27 +0300

    Добавьте файлы проекта.

---
+++ Борисова ПР3 вар 1.7/Mod.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Борисова_ПР3_вар_1._7
+{
+    abstract class Mod
+    {
+        public abstract void nazv();//вывод названия
+        public abstract double ball();//подсчет среднего балла
+        public abstract void info();//вывод среднего балла
+    }
+    class Exam: Mod
+    {
+        public string nazvanie;//название дисциплины
+        public int five;//количество пятерок за экзамен
+        public int four;//количество четверок за экзамен
+        public int three;//количество троек за экзамен
+        public int two;//количество двоек за экзамен
+        public double kol;//количество студентов, сдавших экзамен
+        public Exam(string nazvanie)
+        {
+            this.nazvanie = nazvanie;
+        }
+        public override void nazv()//выводи название дисциплины, по которому был экзамен
+        {
+            Console.WriteLine("Вы сдаете экзамен по дисциплине "+nazvanie);
+        }
+        public override double ball()//подсчет среднего балла
+        {
+            kol = (5 * five + 4 * four + 3 * three + 2 * two) / (five + four + three + two);
+            return kol;
+        }
+        public override void info()//вывод среднего балла
+        {
+            Console.WriteLine("Средний балл группы: "+ kol);
+            
+        }
+    }
+    class ContrWork: Exam
+    {
+        public string predm;//название предмета
+        public int first;//первая оценка студента за контрольную работу
+        public int second;//вторая оценка студента за контрольную работу
+        public double itball;//итоговая оценка студента за контрольную работу
+        public ContrWork(string predm):base(predm)
+        {
+            this.predm = predm;
+        }
+        public override void nazv()//выводи название дисциплины, по которому была контрольная работа
+        {
+            Console.WriteLine("Вы пишете контрольную работу по " + predm);
+        }
+        new public void info()//вывод количества студентов, которые написали контрольную работу
+        {
+            base.info();
+            Console.WriteLine("Написали контрольную работу "+ kolich() + " студентов");
+        }
+        public double kolich()//подсчет количесва студентов, которые написали контрольную работу
+        {
+            return five + four + three + two;
+        }
+        public void yourBall()// вывод итоговой оценки за контрольную работу
+        {
+            itball = (first + second)/2;
+            Console.WriteLine("Ваш балл: "+itball);
+        }
+    }
+}
---
+++ Борисова ПР3 вар 1.7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Борисова_ПР3_вар_1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название дисциплины");
+            string nazvanie = Console.ReadLine();
+            Exam A = new Exam(nazvanie);
+            A.nazv();
+            Console.WriteLine("Введите количество пятерок: ");
+            A.five = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество четверок: ");
+            A.four = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество троек: ");
+            A.three = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество двоек: ");
+            A.two = Convert.ToInt32(Console.ReadLine());
+            A.info();
+            Console.WriteLine("Введите название предмета");
+            string predm = Console.ReadLine();
+            ContrWork B = new ContrWork(predm);
+            B.nazv();
+            B.info();
+            Console.WriteLine("Введите вашу первую оценку за контрольную работу: ");
+            B.first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите вашу вторую оценку за контрольную работу: ");
+            B.second = Convert.ToInt32(Console.ReadLine());
+            B.yourBall();
+
+        }
+    }
+}
